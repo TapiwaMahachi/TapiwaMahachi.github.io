5597a3c3eac69760463bd56c9bddcfd7f47b0dc4
Tapiwa <mahachitapiwa13@gmail.com>
2021-01-28 05:25:37 +0200

    Updates

---
+++ precache-manifest.4da1d34bcfc696087a1381497bf85980.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de0e7322c7bf20805668475ed7662064",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ba0c9a8c6da047aabe1d",
+    "url": "/portfolio/static/css/main.51f9dbf6.chunk.css"
+  },
+  {
+    "revision": "96b63ac208a0d404863e",
+    "url": "/portfolio/static/js/2.4c9726f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.4c9726f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba0c9a8c6da047aabe1d",
+    "url": "/portfolio/static/js/main.17b7ab03.chunk.js"
+  },
+  {
+    "revision": "272c84328c4e9a9d0bda",
+    "url": "/portfolio/static/js/runtime-main.6093c233.js"
+  },
+  {
+    "revision": "ceecc4fe0b3f924ae9ba61e23526897b",
+    "url": "/portfolio/static/media/me.ceecc4fe.png"
+  },
+  {
+    "revision": "ccf7929e9539fd50b2e406bcd279bf11",
+    "url": "/portfolio/static/media/todo.ccf7929e.PNG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.9a17ce968933bdc8e36bbbb66dbf5496.js"
+  "/portfolio/precache-manifest.4da1d34bcfc696087a1381497bf85980.js"
 );
 
 self.addEventListener('message', (event) => {
